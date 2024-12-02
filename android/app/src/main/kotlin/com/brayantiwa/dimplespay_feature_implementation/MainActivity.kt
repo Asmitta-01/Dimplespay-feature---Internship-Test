8cccc9cb7abf2d60109c29f799a6af410f82b192
Asmitta <tiwabrayan@gmail.com>
2024-12-02 08:46:56 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/brayantiwa/dimplespay_feature_implementation/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.brayantiwa.dimplespay_feature_implementation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utils/theme.dart
@@ -0,0 +1,251 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static TextDirection textDirection = TextDirection.ltr;
+
+  static ThemeData appTheme = lightTheme.copyWith(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: const Color(0xff006399),
+      brightness: Brightness.light,
+      primary: const Color(0xff006399),
+      secondary: const Color(0xffe8d302),
+      onSurface: Colors.black87,
+      surface: Colors.white,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        elevation: 0,
+        foregroundColor: Colors.white,
+        backgroundColor: const Color(0xff006399),
+        padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 10.0),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        textStyle: const TextStyle(
+          fontFamily: 'AfacadFlux',
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
+          letterSpacing: 0.15,
+        ),
+      ),
+    ),
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(
+        elevation: 0,
+        side: const BorderSide(color: Color(0xff006399), width: 1.4),
+        // foregroundColor: Colors.white,
+        // backgroundColor: const Color(0xff006399),
+        padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 10.0),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        textStyle: const TextStyle(
+          fontFamily: 'AfacadFlux',
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
+          letterSpacing: 0.15,
+        ),
+      ),
+    ),
+  );
+
+  AppTheme._();
+
+  static final ThemeData lightTheme = ThemeData(
+    /// Brightness
+    brightness: Brightness.light,
+
+    /// Primary Color
+    primaryColor: const Color(0xff3C4EC5),
+    scaffoldBackgroundColor: const Color(0xffffffff),
+    canvasColor: Colors.transparent,
+
+    /// AppBar Theme
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color(0xffffffff),
+      iconTheme: IconThemeData(color: Color(0xff495057)),
+      actionsIconTheme: IconThemeData(color: Color(0xff495057)),
+    ),
+
+    // Drawer Theme
+    drawerTheme: const DrawerThemeData(backgroundColor: Color(0xffffffff)),
+
+    /// Card Theme
+    cardTheme: const CardTheme(color: Color(0xfff0f0f0)),
+    cardColor: const Color(0xfff0f0f0),
+
+    fontFamily: 'AfacadFlux',
+    textTheme: _getTextTheme(),
+
+    /// Floating Action Theme
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: const Color(0xff3C4EC5),
+        splashColor: const Color(0xffeeeeee).withAlpha(100),
+        highlightElevation: 8,
+        elevation: 4,
+        focusColor: const Color(0xff3C4EC5),
+        hoverColor: const Color(0xff3C4EC5),
+        foregroundColor: const Color(0xffeeeeee)),
+
+    // InputDecorationTheme
+    inputDecorationTheme: const InputDecorationTheme(
+      hintStyle: TextStyle(
+        fontFamily: 'AfacadFlux',
+        fontSize: 19,
+        letterSpacing: 0.15,
+      ),
+      labelStyle: TextStyle(
+        fontFamily: 'AfacadFlux',
+        fontSize: 19,
+        letterSpacing: 0.15,
+      ),
+      errorStyle: TextStyle(
+        fontFamily: 'AfacadFlux',
+        fontSize: 16,
+        letterSpacing: 0.15,
+      ),
+      suffixIconColor: Color(0xff3C4EC5),
+    ),
+
+    /// Divider Theme
+    dividerTheme:
+        const DividerThemeData(color: Color(0xffe8e8e8), thickness: 1),
+    dividerColor: const Color(0xffe8e8e8),
+
+    /// Bottom AppBar Theme
+    bottomAppBarTheme:
+        const BottomAppBarTheme(color: Color(0xffeeeeee), elevation: 2),
+
+    /// Tab bar Theme
+    tabBarTheme: const TabBarTheme(
+      unselectedLabelColor: Color(0xff495057),
+      labelColor: Color(0xff3d63ff),
+      indicatorSize: TabBarIndicatorSize.label,
+      indicator: UnderlineTabIndicator(
+        borderSide: BorderSide(color: Color(0xff3d63ff), width: 2.0),
+      ),
+    ),
+
+    /// Radio theme
+    radioTheme: RadioThemeData(
+      fillColor: WidgetStateProperty.all(const Color(0xff3C4EC5)),
+    ),
+
+    ///Switch Theme
+    switchTheme: SwitchThemeData(
+      trackColor: WidgetStateProperty.resolveWith((state) {
+        const Set<WidgetState> interactiveStates = <WidgetState>{
+          WidgetState.pressed,
+          WidgetState.hovered,
+          WidgetState.focused,
+          WidgetState.selected,
+        };
+        if (state.any(interactiveStates.contains)) {
+          return const Color(0x990A6FB8);
+        }
+        return null;
+      }),
+      thumbColor: WidgetStateProperty.resolveWith((state) {
+        const Set<WidgetState> interactiveStates = <WidgetState>{
+          WidgetState.pressed,
+          WidgetState.hovered,
+          WidgetState.focused,
+          WidgetState.selected,
+        };
+        if (state.any(interactiveStates.contains)) {
+          return const Color(0xff3C4EC5);
+        }
+        return null;
+      }),
+    ),
+
+    /// Slider Theme
+    sliderTheme: SliderThemeData(
+      activeTrackColor: const Color(0xff3d63ff),
+      inactiveTrackColor: const Color(0xff3d63ff).withAlpha(140),
+      trackShape: const RoundedRectSliderTrackShape(),
+      trackHeight: 4.0,
+      thumbColor: const Color(0xff3d63ff),
+      thumbShape: const RoundSliderThumbShape(enabledThumbRadius: 10.0),
+      overlayShape: const RoundSliderOverlayShape(overlayRadius: 24.0),
+      tickMarkShape: const RoundSliderTickMarkShape(),
+      inactiveTickMarkColor: Colors.red[100],
+      valueIndicatorShape: const PaddleSliderValueIndicatorShape(),
+      valueIndicatorTextStyle: const TextStyle(
+        color: Color(0xffeeeeee),
+      ),
+    ),
+
+    /// Other Colors
+    splashColor: Colors.white.withAlpha(100),
+    indicatorColor: const Color(0xffeeeeee),
+    highlightColor: const Color(0xffeeeeee),
+    colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color(0xff3C4EC5), brightness: Brightness.light)
+        .copyWith(surface: const Color(0xffffffff))
+        .copyWith(error: const Color(0xfff0323c)),
+  );
+
+  static TextTheme _getTextTheme() {
+    return const TextTheme(
+      displayLarge: TextStyle(fontFamily: 'AfacadFlux', fontSize: 99),
+      displayMedium: TextStyle(fontFamily: 'AfacadFlux', fontSize: 62),
+      displaySmall: TextStyle(fontFamily: 'AfacadFlux', fontSize: 50),
+      headlineMedium: TextStyle(fontFamily: 'AfacadFlux', fontSize: 42.5),
+      headlineSmall: TextStyle(
+        fontFamily: 'AfacadFlux',
+        fontSize: 30,
+        fontWeight: FontWeight.w400,
+      ),
+      titleLarge: TextStyle(
+        fontFamily: 'AfacadFlux',
+        fontSize: 26,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 0.15,
+      ),
+      titleMedium: TextStyle(
+        fontFamily: 'AfacadFlux',
+        fontSize: 22.5,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.15,
+      ),
+      titleSmall: TextStyle(
+        fontFamily: 'AfacadFlux',
+        fontSize: 20,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.1,
+      ),
+      labelLarge: TextStyle(
+        fontFamily: 'AfacadFlux',
+        fontSize: 17.5,
+        fontWeight: FontWeight.w500,
+      ),
+      labelMedium: TextStyle(
+        fontFamily: 'AfacadFlux',
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        letterSpacing: .4,
+      ),
+      labelSmall: TextStyle(
+        fontFamily: 'AfacadFlux',
+        fontSize: 11.5,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 1.5,
+      ),
+      bodyLarge: TextStyle(
+        fontFamily: 'AfacadFlux',
+        fontSize: 20,
+        fontWeight: FontWeight.w500,
+      ),
+      bodyMedium: TextStyle(
+        fontFamily: 'AfacadFlux',
+        fontSize: 17,
+        height: 1.15,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.25,
+      ),
+      bodySmall: TextStyle(
+        fontFamily: 'AfacadFlux',
+        fontSize: 15,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.4,
+      ),
+    );
+  }
+}
